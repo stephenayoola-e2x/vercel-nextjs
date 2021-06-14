80b1325b3e47913904711b674f04442e6f7e1268
Tom Heather <tom.heather@e2x.com>
2021-06-14 10:57:21 +0100

    inital commit

---
+++ next.config.js
@@ -0,0 +1,67 @@
+// const { ContentClient } = require('dc-delivery-sdk-js');
+
+// const client = new ContentClient({
+//     account: process.env.ACCOUNT_NAME,
+//     hubName: "KATA-Hub1"
+//     // baseUrl: "https://cdn.c1.amplience.net/" // process.env.BASE_URL
+// });
+
+// async function generateBlogPages() {
+//     const routeContentReference = process.env.DYNAMIC_CONTENT_REFERENCE_ID;
+//     // fetching your content item by id and return a list of references to blog posts
+//     const data = (await client.getContentItemByKey("blog-list")).toJSON()
+//     console.log('data', data)
+//     const { blogPosts } = data;
+
+//     return blogPosts.reduce((pages, blog) => {
+//         const slug = encodeURIComponent('blog/id');
+//         const blogId = blog._meta.deliveryId;
+//         // creating a page map with the requested data to fetch that blog
+
+//         return Object.assign(pages, {
+//             [`/blog/${slug}`]: {
+//                 page: '/blog',
+//                 query: { blogId, slug }
+//             }
+//         });
+//     }, {});
+// }
+
+const fetch = require('node-fetch');
+
+async function generatePages() {
+    const response = await fetch('http://api.openweathermap.org/data/2.5/find?lat=51.48&lon=-0.16&cnt=10&appid=ce1120e3ba8e41bfa8184eff931c3d8c');
+    const data = await response.json();
+    console.log('data', data)
+
+    const { list } = data;
+
+    return list.reduce((pages, location) => {
+        const sanitisedName = location.name.toLowerCase();
+        const slug = encodeURIComponent(sanitisedName);
+        const id = location.id;
+        // creating a page map with the requested data to fetch that blog
+
+        return Object.assign(pages, {
+            [`/location/${slug}`]: {
+                page: '/location',
+                query: { id, slug }
+            }
+        });
+    }, {});
+}
+
+module.exports = {
+    env: {
+        account: process.env.ACCOUNT_NAME,
+        baseUrl: process.env.BASE_URL
+    },
+    exportPathMap: async () => {
+        const pages = await generatePages();
+        return Object.assign({}, pages, {
+            '/': {
+                page: '/'
+            }
+        });
+   }
+}
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getServerSideProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('http://api.openweathermap.org/data/2.5/weather?q=london&appid=ce1120e3ba8e41bfa8184eff931c3d8c&units=metric')
+  const data = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default function Page2({data}) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Page 2</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Weather in {data.name}</h1>
+        <h2>{data.weather[0].description}, {data.main.temp}°C</h2>
+        <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} />
+      </main>
+    </div>
+  )
+}
