c91de9764673efd109ec18b992d9c9de36999520
tom.heather <tom.heather@e2x.com>
2021-06-15 15:11:46 +0100

    ISR ?

---
+++ pages/strain-isr/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+
+export async function getStaticProps({params}) {
+  const res = await fetch(`https://random-data-api.com/api/cannabis/random_cannabis`)
+  const data = await res.json()
+
+  return {
+    props: {
+      data,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 30, // In seconds
+  }
+}
+
+export default function LocationPage({data}) {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>ISR Strain</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main className={styles.main}>
+          <h1>Random Strain</h1>
+          <h2>{data.strain}, {data.health_benefit}</h2>
+        </main>
+      </div>
+    )
+  }
+  
