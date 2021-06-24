90341194750690121b784ea08838c8de44989fa1
tom.heather <tom.heather@e2x.com>
2021-06-24 20:31:21 +0100

    test

---
+++ pages/strapi/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+
+export default function StrapiPage() {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>test</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+  
+        <main className={styles.main}>
+          test
+        </main>
+      </div>
+    )
+  }
+  
